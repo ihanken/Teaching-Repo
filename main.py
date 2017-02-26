409a15505934d8cc3f5e4b86fb685de16106e787
ihanken <ihanken@bellsouth.net>
2017-02-25 21:27:28 -0600

    Added the palindrome function.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #/usr/bin/python
 
-if __name__ == "__main__":
-    print("Hello, world!")
+def isPal(s):
+    if len(s) < 2: return True
 
-    print("Brandon Ellis")
-    
-    print("Ian Hanken")
+    for i in range(len(s) // 2):
+        if s[i] != s[-1 * (i + 1)]: return False
+
+    return True
+
+if __name__ == "__main__":
+    print(isPal("a"))
+    print(isPal("ab"))
+    print(isPal("aba"))
+    print(isPal("abba"))
+    print(isPal("abcba"))
+    print(isPal("abccc"))
